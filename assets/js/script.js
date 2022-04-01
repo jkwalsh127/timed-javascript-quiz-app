f618e6779c1e2866fff7d68ccb74a0e053e74490
Jake Walsh <jkwalsh127@gmail.com>
2022-04-01 09:01:17 -0700

    initiate button functionality

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var beginButton = document.querySelector("#begin");
+
+beginButton.addEventListener("click", beginQuiz);
+
+// var questionCard = document.createElement("article");
+// questionCard.setAttribute("style", "background-color: red; color: white; width: 100%; height: 100%;")
+// questionCard.textContent = "The quiz has begun";
+
+function beginQuiz() {
+    console.log("working")
+};
