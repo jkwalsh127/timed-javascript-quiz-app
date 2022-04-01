85377c14c0d681a79c058ac253e59fd96f4e0612
Jake Walsh <jkwalsh127@gmail.com>
2022-04-01 09:18:26 -0700

    initiate timer upon button selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,20 @@
 var beginButton = document.querySelector("#begin");
 
-beginButton.addEventListener("click", beginQuiz);
+var timeEl = document.getElementById("time");
+var timeRemaining = 5;
+
+beginButton.addEventListener("click", keepTime);
+
+function keepTime() {
+    var timeTracker = setInterval(function() {
+        timeRemaining--;
+        timeEl.textContent = timeRemaining + " remaining";
+        if (timeRemaining === 0) {
+            clearInterval(timeTracker); 
+            alert("Good Job");
+        }
+    }, 1000);
+}
 
 // var questionCard = document.createElement("article");
 // questionCard.setAttribute("style", "background-color: red; color: white; width: 100%; height: 100%;")
