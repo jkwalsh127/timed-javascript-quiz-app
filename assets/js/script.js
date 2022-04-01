398e0640b1bacb5f823baa0326e26900f8bc3db8
Jake Walsh <jkwalsh127@gmail.com>
2022-04-01 09:52:16 -0700

    add onclick function to button to append question card

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,22 +4,28 @@ var timeEl = document.getElementById("time");
 var timeRemaining = 5;
 
 beginButton.addEventListener("click", keepTime);
+beginButton.addEventListener("click", beginQuiz);
+
 
 function keepTime() {
     var timeTracker = setInterval(function() {
         timeRemaining--;
         timeEl.textContent = timeRemaining + " remaining";
         if (timeRemaining === 0) {
+            timeEl.textContent = "time's up";
             clearInterval(timeTracker); 
             alert("Good Job");
         }
     }, 1000);
 }
 
-// var questionCard = document.createElement("article");
-// questionCard.setAttribute("style", "background-color: red; color: white; width: 100%; height: 100%;")
-// questionCard.textContent = "The quiz has begun";
+
 
 function beginQuiz() {
-    console.log("working")
+    beginButton.setAttribute("style", "display: none")
+    var questionCard = document.createElement("article");
+    questionCard.setAttribute("style", "background-color: red; color: white; width: 100%; height: 100%;")
+    questionCard.textContent = "The quiz has begun";
+    document.body.children[1].children[0].appendChild(questionCard);
+
 };
