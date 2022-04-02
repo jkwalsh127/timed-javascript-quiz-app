0314d0de9e5dd2a02e7b5775ecea67dc85d024ef
Jake Walsh <jkwalsh127@gmail.com>
2022-04-02 00:28:03 -0700

    add function to assign each button a correct or incorrect value with each question

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,19 +5,23 @@ var answerbtn1 = document.querySelector("#btn1");
 var answerbtn2 = document.querySelector("#btn2");
 var answerbtn3 = document.querySelector("#btn3");
 var answerbtn4 = document.querySelector("#btn4");
+
+var questionPrompt = document.querySelector("#question-prompt")
+
 answerbtn1.addEventListener("click", function(event) {
-    var element = event.target;
-    if (element.matches("button")) {
-        var state = element.getAttribute("data-state");
+
+    // var element = event.target;
+    // if (element.matches("button")) {
+        var state = event.target.getAttribute("data-state");
         if (state === "correct") {
-            element.setAttribute("class", "correct");
+            // element.setAttribute("class", "correct");
             console.log("correct answer")
         } else {
-            element.setAttribute("class", "incorrect")
+            // element.setAttribute("class", "incorrect")
             console.log("incorrect answer")
         }
     }
-});
+);
 
 var timeEl = document.getElementById("time");
 
@@ -39,9 +43,38 @@ function beginQuiz() {
 
     keepTime();
 
+    firstQuestion();
+
     beginButton.setAttribute("style", "display: none")
     var questionCard = document.createElement("article");
     questionCard.setAttribute("style", "background-color: red; color: white; width: 100%; height: 100%;");
     questionCard.textContent = "The quiz has begun";
     document.body.children[1].children[0].appendChild(questionCard);
 };
+
+// function setCorrect(event) {
+//     var element = event.target;
+//     // event.stopPropagation();
+//     element.setAttribute("data-state", "correct");
+// };
+
+// function setIncorrect(event) {
+//     var element = event.target;
+//     element.setAttribute("data-state", "incorrect");
+// };
+
+function firstQuestion() {
+    answerbtn1.setAttribute("class", "correct");
+    answerbtn2.setAttribute("class", "incorrect");
+    answerbtn3.setAttribute("class", "incorrect");
+    answerbtn4.setAttribute("class", "incorrect");
+};
+
+    // var state = element.getAttribute("data-state");
+    // if (state === "neutral") {
+    //     element.setAttribute("data-name", "data-correct");
+    // } else {
+    //     element.setAttribute("data-name", "data-neutral");
+    //     console.log("neutral")
+    // }
+
